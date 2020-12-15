59ea6c0e29e4d0328387bd2535e98c0911f570fa
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-12-15 17:00:12 +0000

    Set up responsiveness

---
+++ lib/src/components/responsive/layout_wrapper.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:personal_web/src/components/responsive/screen_type_layout.dart';
+import 'package:personal_web/src/main_page.dart';
+
+class LayoutWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
+      tablet: MainPage(),
+    );
+  }
+}
