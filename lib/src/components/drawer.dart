1302c93cedec2c57bd47d28f3316d7c64ab0d35a
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-12-16 13:58:31 +0000

    Add a close icon to drawer

--- lib/src/components/drawer.dart
+++ lib/src/components/drawer.dart
@@ -42,71 +42,89 @@ class _AppDrawerState extends State<AppDrawer> {
     return Drawer(
       child: Container(
         color: Colors.teal,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: items.map((item) {
-            return InkWell(
-              onTap: () {
-                switch (item) {
-                  case 'Home':
-                  case 'About':
-                    setState(() {
-                      selectedIndex = items.indexOf(item);
-                    });
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              left: 8,
+              top: 8,
+              child: IconButton(
+                color: Colors.white,
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: items.map((item) {
+                return InkWell(
+                  onTap: () {
+                    switch (item) {
+                      case 'Home':
+                      case 'About':
+                        setState(() {
+                          selectedIndex = items.indexOf(item);
+                        });
 
-                    if (widget.onItemSelected != null) {
-                      widget.onItemSelected(selectedIndex);
+                        if (widget.onItemSelected != null) {
+                          widget.onItemSelected(selectedIndex);
+                        }
+                        break;
+                      case 'Resume':
+                        break;
+                      case 'Blog':
+                        UrlHelper.launchUrl(
+                          'https://medium.com/@debrahkwesibuabeng2',
+                        );
+                        break;
+                      default:
+                      // setState(() {
+                      //   selectedIndex = items.indexOf(item);
+                      // });
+
+                      // if (widget.onItemSelected != null) {
+                      //   widget.onItemSelected(selectedIndex);
+                      // }
                     }
-                    break;
-                  case 'Resume':
-                    break;
-                  case 'Blog':
-                    UrlHelper.launchUrl(
-                      'https://medium.com/@debrahkwesibuabeng2',
-                    );
-                    break;
-                  default:
-                  // setState(() {
-                  //   selectedIndex = items.indexOf(item);
-                  // });
 
-                  // if (widget.onItemSelected != null) {
-                  //   widget.onItemSelected(selectedIndex);
-                  // }
-                }
-
-                Navigator.pop(context);
-              },
-              child: item == 'Resume'
-                  ? SizedBox(width: 160, child: ResumeButton())
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          item,
-                          style: textStyle.copyWith(
-                            fontWeight: FontWeight.w500,
-                            color: textStyle.color.withOpacity(
-                              selectedIndex == items.indexOf(item) ? 1.0 : 0.75,
+                    Navigator.pop(context);
+                  },
+                  child: item == 'Resume'
+                      ? SizedBox(width: 160, child: ResumeButton())
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              item,
+                              style: textStyle.copyWith(
+                                fontWeight: FontWeight.w500,
+                                color: textStyle.color.withOpacity(
+                                  selectedIndex == items.indexOf(item)
+                                      ? 1.0
+                                      : 0.75,
+                                ),
+                              ),
                             ),
-                          ),
+                            SizedBox(height: 4),
+                            if (item != 'Resume')
+                              AnimatedContainer(
+                                duration: Duration(milliseconds: 300),
+                                height: 2,
+                                width: 20,
+                                color: selectedIndex == items.indexOf(item)
+                                    ? Colors.white
+                                    : Colors.transparent,
+                              ),
+                            SizedBox(height: 20),
+                          ],
                         ),
-                        SizedBox(height: 4),
-                        if (item != 'Resume')
-                          AnimatedContainer(
-                            duration: Duration(milliseconds: 300),
-                            height: 2,
-                            width: 20,
-                            color: selectedIndex == items.indexOf(item)
-                                ? Colors.white
-                                : Colors.transparent,
-                          ),
-                        SizedBox(height: 20),
-                      ],
-                    ),
-            );
-          }).toList(),
+                );
+              }).toList(),
+            ),
+          ],
         ),
       ),
     );
